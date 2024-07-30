13d12085fdea41e4d9338be03323fceadf330d12
Felipe Eduardo <felipeedaurdogit@gmail.com>
2024-07-30 11:05:03 -0300

    Criando a tabela Post

---
+++ pw3-blog/src/main/java/br/com/etechoracio/blog/entity/Post.java
@@ -0,0 +1,29 @@
+package br.com.etechoracio.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.cglib.core.Local;
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@Entity
+@Table(name = "TBL_POST")
+public class Post {
+
+    @Id
+    @Column(name = "ID_POST")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "TX_TITULO")
+    private String titulo;
+
+    @Column(name = "TX_CONTEUDO")
+    private String conteudo;
+
+    @Column(name = "DT_CRIACAO")
+    private LocalDateTime dataCriacao;
+
+
+}
